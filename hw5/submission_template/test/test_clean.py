007e021704919f32d165246f650746cbf2226d7d
42694647426 <hyyshao@gmail.com>
2021-10-29 14:59:19 -0400

    unit test

--- hw5/submission_template/test/test_clean.py
+++ hw5/submission_template/test/test_clean.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 import os, sys
 import json
-import pytest
+import tempfile
 parentdir = Path(__file__).parents[1]
 sys.path.append(parentdir)
 
@@ -24,43 +24,56 @@ class CleanTest(unittest.TestCase):
         self.test_5 = 'test/fixtures/test_5.json'
         self.test_6 = 'test/fixtures/test_6.json'
         
-    def test_clean_file(self):
+    def test_0(self):
         print("\nRUNNING TESTS FOR HW5 - clean.py")
         print("Ensure txt file exists")
         self.assertEqual(os.path.isfile(self.clean_file), True)
         print("OK")
     
-    def test_format(self):
-        os.system(f'python src/clean.py -i {self.test_3} -o {self.test_3}')
-        assert os.stat(self.test_3).st_size == 0
+    def test_3(self):
+        temp =  tempfile.NamedTemporaryFile(delete=False)
+        os.system(f'python src/clean.py -i {self.test_3} -o {temp.name}')
+        assert os.stat(temp.name).st_size == 0
         print("Pass test 3!")
+        temp.close()
+        
     
-    def test_title(self):
-        os.system(f'python src/clean.py -i {self.test_1} -o {self.test_1}')
-        assert os.stat(self.test_1).st_size == 0
+    def test_1(self):
+        temp =  tempfile.NamedTemporaryFile(delete=False)
+        os.system(f'python src/clean.py -i {self.test_1} -o {temp.name}')
+        assert os.stat(temp.name).st_size == 0
         print("Pass test 1!")
-    
-    def test_date(self):
-        os.system(f'python src/clean.py -i {self.test_2} -o {self.test_2}')
-        assert os.stat(self.test_2).st_size == 0
-        print("Pass test 2!")
-    
-    
-    def test_author(self):
-        os.system(f'python src/clean.py -i {self.test_4} -o {self.test_4}')
-        assert os.stat(self.test_4).st_size == 0
+        temp.close()
+        
+    def test_4(self):
+        temp =  tempfile.NamedTemporaryFile(delete=False)
+        os.system(f'python src/clean.py -i {self.test_4} -o {temp.name}')
+        assert os.stat(temp.name).st_size == 0
         print("Pass test 4!")
+        temp.close()
+
+    def test_2(self):
+        temp =  tempfile.NamedTemporaryFile(delete=False)
+        os.system(f'python src/clean.py -i {self.test_2} -o {temp.name}')
+        assert os.stat(temp.name).st_size == 0
+        print("Pass test 2!")
+        temp.close()
     
-    def test_count(self):
-        os.system(f'python src/clean.py -i {self.test_5} -o {self.test_5}')
-        assert os.stat(self.test_5).st_size == 0
+    
+    def test_5(self):
+        temp =  tempfile.NamedTemporaryFile(delete=False)
+        os.system(f'python src/clean.py -i {self.test_5} -o {temp.name}')
+        assert os.stat(temp.name).st_size == 0
         print("Pass test 5!")
+        temp.close()
     
-    def test_json(self):
-        os.system(f'python src/clean.py -i {self.test_6} -o {self.test_6}')
-        dict = load_json(self.test_6)
+    def test_6(self):
+        temp =  tempfile.NamedTemporaryFile(delete=False)
+        os.system(f'python src/clean.py -i {self.test_6} -o {temp.name}')
+        dict = load_json(temp.name)
         assert (not " " in x for x in dict['tags'])
         print("Pass test 6!")
+        temp.close()
         
     
 if __name__ == '__main__':
