771a51c14714f83fa9aa0749ded420d2767dcd32
42694647426 <hyyshao@gmail.com>
2021-10-28 20:46:03 -0400

    hw5

--- hw5/submission_template/src/clean.py
+++ hw5/submission_template/src/clean.py
@@ -1,24 +1,85 @@
 import argparse
 import os, sys, json
 from zipfile import ZipFile
-
+from datetime import datetime
+import pytz
+from dateutil.parser import isoparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', help='input file(JSON file)', required=True)
 parser.add_argument('-o', help='output file(JSON file)', required=True)
 args = parser.parse_args()
 INPUTFILE = '{}'.format(args.i) 
-OUTPUTFILE = '{}'.format(args.i) 
+OUTPUTFILE = '{}'.format(args.o) 
 
-def clean(input, output):
-    for i in input:
-        print(i)
-    return
+def clean(json_lst, output):
+    pos = 0
+    if not json_lst:
+        open(output, "w").close()
+        return
+    for line in json_lst:
+        #1
+        if "title" in line or "title_text" in line:
+            if "title_text" in line:
+                line["title"] = line.pop("title_text")   
+        else:
+            #print("delete for #1")
+            continue
+        #2
+        try:
+            dt = isoparse(line["createdAt"])
+        except Exception as e:
+            #print("delete for #2")
+            #print(e)
+            continue
+        else:
+            line["createdAt"] = dt.replace().astimezone(pytz.utc).strftime("%Y-%m-%dT%H:%M:%S%z")
+        
+        #3
+        if not line["author"] or line["author"].casefold() == 'N/A'.casefold() or line["author"].casefold() == 'null'.casefold():
+            #print("delete for #3")
+            continue
+        #4
+        if 'total_count' in line:
+            if not (type(line['total_count']) == int or type(line['total_count']) == str or type(line['total_count']) == float):
+                #print("delete for #4")
+                continue
+            try:
+                int(line['total_count'])
+            except:
+                #print("delete for #4")
+                continue
+            else:
+                line['total_count'] = int(line['total_count'])
+        
+        if 'tags' in line:
+            temp = []
+            for i in line['tags']:
+                temp = temp + i.split()
+            line['tags'] = temp
+
+        json_lst[pos] = line
+        pos +=1
+    
+    del json_lst[pos:]
+    with open(output, 'w') as f:
+        for line in json_lst:
+            f.write(json.dumps(line))
+    return  
+
 def main():
-    f = open(INPUTFILE)
-    input =  json.load(f)
-    clean(input, OUTPUTFILE)
-    print(f"{OUTPUTFILE} file created successfully >3<")
+    input_lst = []
+    with open (INPUTFILE, 'r') as file:
+        for line in file:
+            try:
+                json.loads(line)
+            except:
+                continue
+            else:
+                input_lst.append(json.loads(line))
+
+    clean(input_lst, OUTPUTFILE)
+    
 
 
 if __name__ == "__main__":
--- hw5/submission_template/test/test_clean.py
+++ hw5/submission_template/test/test_clean.py
@@ -1,17 +1,66 @@
 import unittest
 from pathlib import Path
 import os, sys
+import json
+import pytest
 parentdir = Path(__file__).parents[1]
 sys.path.append(parentdir)
 
 
-class CleanTest(unittest.TestCase):
-    # def setUp(self):
-        # You might want to load the fixture files as variables, and test your code against them. Check the fixtures folder.
-        
+def load_json(filename):
+    with open(filename, 'r') as f:
+        for i in f:
+            return json.loads(i)
 
-    # def test_title(self):
-        # Just an idea for a test; write your implementation
+
+class CleanTest(unittest.TestCase):
+
+    def setUp(self):
+        self.clean_file = os.path.join(parentdir, 'src/clean.py')
+        self.test_1 = 'test/fixtures/test_1.json'
+        self.test_2 = 'test/fixtures/test_2.json'
+        self.test_3 = 'test/fixtures/test_3.json'
+        self.test_4 = 'test/fixtures/test_4.json'
+        self.test_5 = 'test/fixtures/test_5.json'
+        self.test_6 = 'test/fixtures/test_6.json'
+        
+    def test_clean_file(self):
+        print("\nRUNNING TESTS FOR HW5 - clean.py")
+        print("Ensure txt file exists")
+        self.assertEqual(os.path.isfile(self.clean_file), True)
+        print("OK")
+    
+    def test_format(self):
+        os.system(f'python src/clean.py -i {self.test_3} -o {self.test_3}')
+        assert os.stat(self.test_3).st_size == 0
+        print("Pass test 3!")
+    
+    def test_title(self):
+        os.system(f'python src/clean.py -i {self.test_1} -o {self.test_1}')
+        assert os.stat(self.test_1).st_size == 0
+        print("Pass test 1!")
+    
+    def test_date(self):
+        os.system(f'python src/clean.py -i {self.test_2} -o {self.test_2}')
+        assert os.stat(self.test_2).st_size == 0
+        print("Pass test 2!")
+    
+    
+    def test_author(self):
+        os.system(f'python src/clean.py -i {self.test_4} -o {self.test_4}')
+        assert os.stat(self.test_4).st_size == 0
+        print("Pass test 4!")
+    
+    def test_count(self):
+        os.system(f'python src/clean.py -i {self.test_5} -o {self.test_5}')
+        assert os.stat(self.test_5).st_size == 0
+        print("Pass test 5!")
+    
+    def test_json(self):
+        os.system(f'python src/clean.py -i {self.test_6} -o {self.test_6}')
+        dict = load_json(self.test_6)
+        assert (not " " in x for x in dict['tags'])
+        print("Pass test 6!")
         
     
 if __name__ == '__main__':
